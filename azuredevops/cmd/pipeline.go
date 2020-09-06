8c912fd758ac21763575991fa673c74c50e2319c
Zuo Yang <zuya@microsoft.com>
2020-09-06 05:37:17 +0000

    add get pipeline, list run, get run command

--- azuredevops/cmd/pipeline.go
+++ azuredevops/cmd/pipeline.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 
 	vsts "github.com/microsoft/azure-devops-go-api/azuredevops"
 	vstspipelines "github.com/microsoft/azure-devops-go-api/azuredevops/pipelines"
@@ -13,7 +15,8 @@ import (
 var (
 	organization        string
 	project             string
-	author              string
+	pipelineID          int
+	runID               int
 	personalAccessToken string
 )
 
@@ -24,7 +27,16 @@ func init() {
 	pipelinesCmd.MarkPersistentFlagRequired("organization")
 	pipelinesCmd.MarkPersistentFlagRequired("project")
 
+	listPipelineRunCmd.Flags().IntVar(&pipelineID, "pipelineid", 0, "pipeline id to retrieve")
+	listPipelineRunCmd.MarkFlagRequired("pipelineid")
+
+	getPipelineRunCmd.Flags().IntVar(&pipelineID, "pipelineid", 0, "pipeline id to retrieve")
+	getPipelineRunCmd.MarkFlagRequired("pipelineid")
+
 	pipelinesCmd.AddCommand(listPipelinesCmd)
+	pipelinesCmd.AddCommand(getPipelineCmd)
+	pipelinesCmd.AddCommand(listPipelineRunCmd)
+	pipelinesCmd.AddCommand(getPipelineRunCmd)
 }
 
 var pipelinesCmd = &cobra.Command{
@@ -69,6 +81,142 @@ var listPipelinesCmd = &cobra.Command{
 				index++
 			}
 		}
+
+		return nil
+	},
+}
+
+var getPipelineCmd = &cobra.Command{
+	Use:           "get [pipelineID]",
+	Short:         "Get detailed information of the specified pipeline",
+	Args:          cobra.ExactArgs(1),
+	SilenceErrors: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		sourcePipelineID, err := strconv.ParseInt(args[0], 10, 64)
+		pipelineID = int(sourcePipelineID)
+
+		if personalAccessToken == "" {
+			value, exists := os.LookupEnv("token")
+			if exists == false {
+				return fmt.Errorf("Please set personal access token or specify it in command line")
+			}
+			personalAccessToken = value
+		}
+
+		organizationURL := fmt.Sprintf("https://dev.azure.com/%s", organization)
+
+		connection := vsts.NewPatConnection(organizationURL, personalAccessToken)
+
+		ctx := context.Background()
+
+		// Create a client to interact with the Pipelines area
+		pipelineClient := vstspipelines.NewClient(ctx, connection)
+
+		responseValue, err := pipelineClient.GetPipeline(ctx, vstspipelines.GetPipelineArgs{
+			Project:    &project,
+			PipelineId: &pipelineID,
+		})
+
+		if err != nil {
+			fmt.Printf("Get pipeline %d failed for project %s, error: %v", pipelineID, project, err)
+			return fmt.Errorf("Get pipeline %d failed for project %s, error: %v", pipelineID, project, err)
+		}
+
+		pipeline, err := json.Marshal(responseValue)
+		if err != nil {
+			return fmt.Errorf("%v", err)
+		}
+
+		fmt.Printf("%v", string(pipeline))
+
+		return nil
+	},
+}
+
+var listPipelineRunCmd = &cobra.Command{
+	Use:   "list-run",
+	Short: "Get all the runs the specified pipeline triggered",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if personalAccessToken == "" {
+			value, exists := os.LookupEnv("token")
+			if exists == false {
+				return fmt.Errorf("Please set personal access token or specify it in command line")
+			}
+			personalAccessToken = value
+		}
+
+		organizationURL := fmt.Sprintf("https://dev.azure.com/%s", organization)
+
+		connection := vsts.NewPatConnection(organizationURL, personalAccessToken)
+
+		ctx := context.Background()
+
+		// Create a client to interact with the Pipelines area
+		pipelineClient := vstspipelines.NewClient(ctx, connection)
+
+		responseValue, err := pipelineClient.ListRuns(ctx, vstspipelines.ListRunsArgs{
+			Project:    &project,
+			PipelineId: &pipelineID,
+		})
+
+		if err != nil {
+			fmt.Printf("Get pipeline runs failed for pipeline %d, error: %v", pipelineID, err)
+			return fmt.Errorf("Get pipeline runs failed for pipeline %d, error: %v", pipelineID, err)
+		}
+
+		// index := 0
+		if responseValue != nil {
+			fmt.Printf("Count = %v\n", len(*responseValue))
+		}
+
+		return nil
+	},
+}
+
+var getPipelineRunCmd = &cobra.Command{
+	Use:           "get-run",
+	Short:         "Get detailed information of the specified pipeline run",
+	Args:          cobra.ExactArgs(1),
+	SilenceErrors: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		sourceRunID, err := strconv.ParseInt(args[0], 10, 64)
+		runID = int(sourceRunID)
+
+		if personalAccessToken == "" {
+			value, exists := os.LookupEnv("token")
+			if exists == false {
+				return fmt.Errorf("Please set personal access token or specify it in command line")
+			}
+			personalAccessToken = value
+		}
+
+		organizationURL := fmt.Sprintf("https://dev.azure.com/%s", organization)
+
+		connection := vsts.NewPatConnection(organizationURL, personalAccessToken)
+
+		ctx := context.Background()
+
+		// Create a client to interact with the Pipelines area
+		pipelineClient := vstspipelines.NewClient(ctx, connection)
+
+		responseValue, err := pipelineClient.GetRun(ctx, vstspipelines.GetRunArgs{
+			Project:    &project,
+			PipelineId: &pipelineID,
+			RunId:      &runID,
+		})
+
+		if err != nil {
+			fmt.Printf("Get pipeline run %d failed for pipeline %d, error: %v", runID, pipelineID, err)
+			return fmt.Errorf("Get pipeline run %d failed for pipeline %d, error: %v", runID, pipelineID, err)
+		}
+
+		run, err := json.Marshal(responseValue)
+		if err != nil {
+			return fmt.Errorf("%v", err)
+		}
+
+		fmt.Printf("%v", string(run))
 
 		return nil
 	},
