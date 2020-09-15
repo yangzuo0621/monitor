d5e727dc58a20b84fc73ce8d05d1b546ee183130
Zuo Yang <zuya@microsoft.com>
2020-09-15 16:47:36 +0800

    storage account

--- azuredevops/go.mod
+++ azuredevops/go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/Azure/azure-sdk-for-go v46.1.0+incompatible
+	github.com/Azure/azure-storage-blob-go v0.10.0 // indirect
 	github.com/Azure/go-autorest/autorest v0.11.6
 	github.com/Azure/go-autorest/autorest/adal v0.9.4
 	github.com/Azure/go-autorest/autorest/to v0.4.0
---
+++ azuredevops/pkg/storageaccount/client.go
@@ -0,0 +1,125 @@
+package storageaccount
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"path"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2018-07-01/storage"
+	"github.com/Azure/azure-storage-blob-go/azblob"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/adal"
+	"github.com/Azure/go-autorest/autorest/azure"
+)
+
+type Client struct {
+	ClientID       string
+	TenantID       string
+	ClientSecret   string
+	ResourceGroup  string
+	AccountName    string
+	SubscriptionID string
+}
+
+var (
+	cloudName        string = "AzurePublicCloud"
+	environment      *azure.Environment
+	blobFormatString = `https://%s.blob.core.windows.net`
+)
+
+func Environment() (*azure.Environment, error) {
+	if environment != nil {
+		return environment, nil
+	}
+
+	env, err := azure.EnvironmentFromName(cloudName)
+	if err != nil {
+		return nil, err
+	}
+	return &env, nil
+}
+
+// GetAzureKeyVaultAuthorizer constructs authorizer for storage account
+func (c *Client) GetAzureKeyVaultAuthorizer() (autorest.Authorizer, error) {
+	cloudEnv, err := Environment()
+	if err != nil {
+		return nil, err
+	}
+	alternateEndpoint, err := url.Parse(cloudEnv.ActiveDirectoryEndpoint)
+	if err != nil {
+		return nil, err
+	}
+	alternateEndpoint.Path = path.Join(c.TenantID, "/oauth2/token")
+
+	oauthConfig, err := adal.NewOAuthConfig(cloudEnv.ActiveDirectoryEndpoint, c.TenantID)
+	if err != nil {
+		return nil, fmt.Errorf("create OAuth config failed")
+	}
+	oauthConfig.AuthorizeEndpoint = *alternateEndpoint
+
+	vaultEndpoint := strings.TrimSuffix(cloudEnv.KeyVaultEndpoint, "/")
+	token, err := adal.NewServicePrincipalToken(*oauthConfig, c.ClientID, c.ClientSecret, vaultEndpoint)
+	if err != nil {
+		return nil, fmt.Errorf("create service principal token failed: %w", err)
+	}
+
+	return autorest.NewBearerAuthorizer(token), nil
+}
+
+// GetStorageAccountClient creates a storage account client
+func (c *Client) GetStorageAccountClient() (*storage.AccountsClient, error) {
+	storageAccountsClient := storage.NewAccountsClient(c.SubscriptionID)
+	auth, err := c.GetAzureKeyVaultAuthorizer()
+	if err != nil {
+		return nil, err
+	}
+	storageAccountsClient.Authorizer = auth
+	return &storageAccountsClient, nil
+}
+
+func (c *Client) GetAccountKeys() (*storage.AccountListKeysResult, error) {
+	accountsClient, err := c.GetStorageAccountClient()
+	if err != nil {
+		return nil, err
+	}
+	result, err := accountsClient.ListKeys(context.Background(), c.ResourceGroup, c.AccountName)
+	return &result, err
+}
+
+func (c *Client) GetAccountPrimaryKey() string {
+	response, err := c.GetAccountKeys()
+	if err != nil {
+		log.Fatalf("failed to list keys: %v", err)
+	}
+	return *(((*response.Keys)[0]).Value)
+}
+
+func (c *Client) GetContainerURL(containerName string) azblob.ContainerURL {
+	key := c.GetAccountPrimaryKey()
+	cred, _ := azblob.NewSharedKeyCredential(c.AccountName, key)
+	p := azblob.NewPipeline(cred, azblob.PipelineOptions{})
+	u, _ := url.Parse(fmt.Sprintf(blobFormatString, c.AccountName))
+	service := azblob.NewServiceURL(*u, p)
+	container := service.NewContainerURL(containerName)
+	return container
+}
+
+func (c *Client) GetContainer(containerName string) (azblob.ContainerURL, error) {
+	container := c.GetContainerURL(containerName)
+
+	_, err := container.GetProperties(context.Background(), azblob.LeaseAccessConditions{})
+	return container, err
+}
+
+func (c *Client) CreateContainer(containerName string) (azblob.ContainerURL, error) {
+	container := c.GetContainerURL(containerName)
+
+	_, err := container.Create(
+		context.Background(),
+		azblob.Metadata{},
+		azblob.PublicAccessContainer)
+	return container, err
+}
