0590293ab610f35e3709ee5776e4720e983afa8f
Zuo Yang <zuya@microsoft.com>
2020-09-21 17:31:28 +0800

    add deployment yaml

--- azuredevops/prepare-resource.sh
+++ azuredevops/prepare-resource.sh
@@ -3,6 +3,8 @@
 resource_group="zuya$(date +'%Y%m%d')rg"
 keyvault_name="zuya$(date +'%Y%m%d')akv"
 storage_acount="zuya$(date +'%Y%m%d')account"
+acr_name="zuya$(date +'%Y%m%d')acr"
+cluster_name="zuya$(date +'%Y%m%d')aks"
 
 exists=$(az group exists --name "$resource_group")
 if [[ "$exists" == false ]]; then
@@ -27,3 +29,22 @@ if [[ "$?" -ne 0 ]]; then
 else
     echo "skip create storage acount: $storage_acount"
 fi
+
+az acr show -n "$acr_name" -g "$resource_group" > /dev/null 2>&1
+if [[ "$?" -ne 0 ]]; then
+    echo "create acr acount: $acr_name"
+    az acr create -n "$acr_name" -g "$resource_group" --sku Standard
+else
+    echo "skip create acr acount: $acr_name"
+fi
+
+az aks show -n "$cluster_name" -g "$resource_group" > /dev/null 2>&1
+if [[ "$?" -ne 0 ]]; then
+    echo "create aks cluster: $cluster_name"
+    az aks create -n "$cluster_name" -g "$resource_group"
+else
+    echo "skip create aks cluster: $cluster_name"
+fi
+
+arc_id=$(az acr show -n "$acr_name" -g "$resource_group" --query "id" --output tsv)
+az aks update -n "$cluster_name" -g "$resource_group" --attach-acr "$arc_id"
