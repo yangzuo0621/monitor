5030ac43c755754635459d61c0528b08a475ac5f
Zuo Yang <zuya@microsoft.com>
2020-09-22 15:30:12 +0800

    refactor data json struct

--- monitor/go.mod
+++ monitor/go.mod
@@ -7,8 +7,12 @@ require (
 	github.com/Azure/azure-storage-blob-go v0.10.0
 	github.com/Azure/go-autorest/autorest v0.11.6
 	github.com/Azure/go-autorest/autorest/adal v0.9.4
+	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.3.0 // indirect
 	github.com/microsoft/azure-devops-go-api/azuredevops v1.0.0-b5
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
-	github.com/yangzuo0621/azure-devops-cmd/azuredevops v0.0.0-20200921093128-0590293ab610
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/sys v0.0.0-20200918174421-af09f7315aff // indirect
 )
--- monitor/pkg/cicd/types.go
+++ monitor/pkg/cicd/types.go
@@ -2,12 +2,50 @@ package cicd
 
 // Data encapsulates the status information about whole CI/CD process
 type Data struct {
-	E2EID         int    `json:"e2e_id"`
-	CommitID      string `json:"commit_id"`
-	BuildID       int    `json:"build_id"`
-	BuildStatus   string `json:"build_status"`
-	BuildResult   string `json:"build_result"`
-	ReleaseID     int    `json:"release_id"`
-	ReleaseDate   string `json:"release_date"`
-	ReleaseStatus string `json:"release_status"`
+	MasterValidation *MasterValidation `json:"e2e_master_validation,omitempty"`
+	AKSBuild         *AKSBuild         `json:"ev2_aks_build,omitempty"`
+	AKSRelease       *AKSRelease       `json:"ev2_aks_release,omitempty"`
+	State            DataState         `json:"state"`
+}
+
+// MasterValidation encapsulates the information about `E2Ev2 AKS RP Master Validation`
+type MasterValidation struct {
+	ID       int    `json:"id"`
+	CommitID string `json:"commit_id"`
+	Branch   string `json:"branch"`
+}
+
+// AKSBuild encapsulates the information about `[EV2] AKS Build` runs
+type AKSBuild struct {
+	ID          int     `json:"id"`
+	BuildStatus *string `json:"status,omitempty"`
+	BuildResult *string `json:"result,omitempty"`
+	Count       int     `json:"count"`
+}
+
+// AKSRelease encapsulates the information about `AKS Release` runs
+type AKSRelease struct {
+	ID int `json:"id"`
+}
+
+type DataState string
+
+type dataStateValuesType struct {
+	NotStart          DataState
+	BuildInProgress   DataState
+	BuildFailed       DataState
+	BuildSucceeded    DataState
+	ReleaseInProgress DataState
+	ReleaseFailed     DataState
+	ReleaseSucceeded  DataState
+}
+
+var DataStateValues = dataStateValuesType{
+	NotStart:          "notStart",
+	BuildInProgress:   "buildInProgress",
+	BuildFailed:       "buildFailed",
+	BuildSucceeded:    "buildSucceeded",
+	ReleaseInProgress: "releaseInProgress",
+	ReleaseFailed:     "releaseFailed",
+	ReleaseSucceeded:  "releaseSucceeded",
 }
