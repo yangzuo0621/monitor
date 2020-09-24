d6a336095ef8a9eb4597ebe1fe1f091526c74bcf
Zuo Yang <zuya@microsoft.com>
2020-09-24 17:50:44 +0800

    use helm chart to deploy

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker build . -t monitor:v1
-docker tag monitor:v1 zuya20200921acr.azurecr.io/monitor:v1
+docker tag monitor:v1 zuya20200924acr.azurecr.io/monitor:v1
 
-az acr login -n zuya20200921acr
-docker push zuya20200921acr.azurecr.io/monitor:v1
+az acr login -n zuya20200924acr
+docker push zuya20200924acr.azurecr.io/monitor:v1
